ede6793e6bbe4c133f8dabd3bb3c1612d44175e4
PlatinumKellerman <platinumm@ukr.net>
2022-04-28 20:29:36 +0300

    start - taks1 - gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,35 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const galleryListEl = document.querySelector('.gallery');
+const newGalleryItem = onNewGalleryItemCreate(galleryItems);
+galleryListEl.insertAdjacentHTML('beforeend', newGalleryItem);
+
+function onNewGalleryItemCreate(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+<li class="gallery__item">
+    <a class="gallery__link" 
+        href= "${original}">
+        <img 
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}"
+        />
+    </a>
+</li>`
+    }).join('');
+    
+}
+
+let gallery = new SimpleLightbox('.gallery a',
+    {
+        captionsData: "alt",
+        captionDelay: 250
+    }
+);
+
