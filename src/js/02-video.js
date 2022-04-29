6d702af57639c31eac02f04d7ba8cf871b6f923a
PlatinumKellerman <platinumm@ukr.net>
2022-04-29 22:04:53 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+const vimeoPlayer = document.querySelector('iframe');
+const player = new Player('vimeo-player');
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+
+const currentTime = () => {
+    console.log(player.getCurrentTime().resolve);
+} 
+
+
+
+player.on('pause', currentTime)
+
+
+// player.on('timeupdate', localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds )))
+
+
+// const playerSeconds = localStorage.getItem(LOCALSTORAGE_KEY);
+
+// if (playerSeconds) {
+//   player.setCurrentTime(parseFloat(playerSeconds));
+// }
+
 
