41a5736e85ba34dd8b8c35e0186ccdd86da69d5c
PlatinumKellerman <platinumm@ukr.net>
2022-04-30 12:42:50 +0300

    finish-task-02-videoplayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const vimeoPlayer = document.querySelector('iframe');
-const player = new Player('vimeo-player');
-const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const player = new Player(vimeoPlayer);
+const PLAYERCURRENTTIME_KEY = "videoplayer-current-time";
+const currentTime = localStorage.getItem(PLAYERCURRENTTIME_KEY);
 
+player.on('timeupdate', throttle(data => {
+    localStorage.setItem(PLAYERCURRENTTIME_KEY, JSON.stringify(data.seconds));
+}, 1000));
 
-const currentTime = () => {
-    console.log(player.getCurrentTime().resolve);
-} 
-
-
-
-player.on('pause', currentTime)
-
-
-// player.on('timeupdate', localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds )))
-
-
-// const playerSeconds = localStorage.getItem(LOCALSTORAGE_KEY);
-
-// if (playerSeconds) {
-//   player.setCurrentTime(parseFloat(playerSeconds));
-// }
-
-
+player.on('loaded', () => {
+    player.setCurrentTime(currentTime);
+})
