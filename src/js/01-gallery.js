843ac7500a4d2538f5bd1fa53cf0b447accf5ca0
PlatinumKellerman <platinumm@ukr.net>
2022-04-30 14:32:26 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,8 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
+// Change code below this line
 
 const galleryListEl = document.querySelector('.gallery');
 const newGalleryItem = onNewGalleryItemCreate(galleryItems);
