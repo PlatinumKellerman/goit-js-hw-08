87b5ff726200dd7fb45bc36c4e1a4077f7ae9974
PlatinumKellerman <90471754+PlatinumKellerman@users.noreply.github.com>
2022-04-30 11:17:27 +0000

    Deploying to gh-pages from @ PlatinumKellerman/goit-js-hw-08@96774c55814fdff624bbd0c8d4049418408cae72 🚀

--- 02-video.25780a8f.js
+++ 02-video.25780a8f.js
@@ -7,4 +7,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=r(require("@vimeo/player")),t=r(require("lodash.throttle"));function r(e){return e&&e.__esModule?e:{default:e}}const o=document.querySelector("iframe"),a=new e.default(o),l="videoplayer-current-time",u=localStorage.getItem(l);a.on("timeupdate",(0,t.default)(e=>{localStorage.setItem(l,JSON.stringify(e.seconds))},1e3)),a.on("loaded",()=>{a.setCurrentTime(u)});
 },{"@vimeo/player":"zU8P","lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/goit-js-hw-08/02-video.d846d512.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.25780a8f.js.map
