96774c55814fdff624bbd0c8d4049418408cae72
PlatinumKellerman <platinumm@ukr.net>
2022-04-30 14:16:29 +0300

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,59 +1,33 @@
+import throttle from 'lodash.throttle';
+
 const form = document.querySelector('.feedback-form');
 const emailLabel = form.firstElementChild;
 const messageLabel = emailLabel.nextElementSibling;
-
 const emailInput = emailLabel.firstElementChild;
 const messageInput = messageLabel.firstElementChild;
-const submitButton = form.lastElementChild;
-
 const STORAGE_KEY = 'feedback-form-state';
-
 const values = {};
 
-
-form.addEventListener('input', onFormInput)
-function onFormInput(e) {
+form.addEventListener('input', throttle((e) => {
     values.email = e.currentTarget.email.value;
     values.message = e.currentTarget.message.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(values));
+}), 3000)
+
+
+addEventListener('DOMContentLoaded', onPageLoaded);
+function onPageLoaded() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+        const savedValues = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    emailInput.value = savedValues.email;
+    messageInput.value = savedValues.message;
+    }
 }
-
-emailInput.addEventListener('input', onEmailInput);
-    function onEmailInput(e) {
-        const emailValue = e.currentTarget.value;
-        console.log(emailValue);
-        localStorage.setItem(STORAGE_KEY, emailValue)
-    }
-function saveMail() {
-    const savedMail = localStorage.getItem(STORAGE_KEY)
-    if (savedMail) {
-        emailInput.value = savedMail;
-    }
-    }
-saveMail()
-
-messageInput.addEventListener('input', onMessageInput);
-    function onMessageInput(e) {
-        const messageValue = e.currentTarget.value;
-        console.log(messageValue);
-        localStorage.setItem(STORAGE_KEY, messageValue)
-    }
-function saveMessage() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY)
-    if (savedMessage) {
-        messageInput.value = savedMessage;
-    }
-    }
-saveMessage()
-
-
-
-
 
 form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(e) {
     e.preventDefault();
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
+    console.log(values);
 }
-console.log(values);
-
